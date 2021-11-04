c8e7026f07829eecaba870f424d7b214a93eb491
Tianyi Wang <wtianyi1204@gmail.com>
2021-11-04 11:45:21 -0700

    Cartesian only

--- neural_slinky/e3nn_models.py
+++ neural_slinky/e3nn_models.py
@@ -117,3 +117,100 @@ class SlinkyForcePredictor(torch.nn.Module):
         #     )
         # else:
         return node_outputs
+
+class SlinkyForcePredictorCartesian(torch.nn.Module):
+    def __init__(
+        self,
+        irreps_node_input,
+        irreps_node_attr,
+        irreps_edge_attr,
+        irreps_node_output,
+        max_radius,
+        num_neighbors,
+        num_nodes,
+        mul=50,
+        layers=3,
+        lmax=2,
+        pool_nodes=True,
+    ) -> None:
+        super().__init__()
+
+        self.lmax = lmax
+        self.max_radius = max_radius
+        self.number_of_basis = 10
+        self.num_nodes = num_nodes
+        self.irreps_edge_attr = o3.Irreps(irreps_edge_attr)
+        self.pool_nodes = pool_nodes
+
+        irreps_node_hidden = o3.Irreps([
+            (mul, (l, p))
+            for l in range(lmax + 1)
+            for p in [-1, 1]
+        ])
+
+        self.mp = MessagePassing(
+            irreps_node_sequence=[irreps_node_input] + layers * [irreps_node_hidden] + [irreps_node_output],
+            irreps_node_attr=irreps_node_attr,
+            irreps_edge_attr=self.irreps_edge_attr + o3.Irreps.spherical_harmonics(lmax),
+            fc_neurons=[self.number_of_basis, 100],
+            num_neighbors=num_neighbors,
+        )
+
+        self.irreps_node_input = self.mp.irreps_node_input
+        self.irreps_node_attr = self.mp.irreps_node_attr
+        self.irreps_node_output = self.mp.irreps_node_output
+
+    def preprocess(self, data: Dict[str, torch.Tensor]) -> torch.Tensor:
+        if 'batch' in data:
+            batch = data['batch']
+        else:
+            batch = data['pos'].new_zeros(data['pos'].shape[0], dtype=torch.long)
+
+        # Create graph
+        if 'edge_src' in data and 'edge_dst' in data:
+            edge_src = data['edge_src']
+            edge_dst = data['edge_dst']
+        else:
+            edge_index = radius_graph(data['pos'], self.max_radius, batch)
+            edge_src = edge_index[0]
+            edge_dst = edge_index[1]
+
+        # Edge attributes
+        edge_vec = data['pos'][edge_src] - data['pos'][edge_dst]
+
+        if 'x' in data:
+            node_input = data['x']
+        else:
+            node_input = data['node_input']
+
+        node_attr = data['node_attr']
+        edge_attr = data['edge_attr']
+
+        return batch, node_input, node_attr, edge_attr, edge_src, edge_dst, edge_vec
+
+    def forward(self, data: Dict[str, torch.Tensor]) -> torch.Tensor:
+        batch, node_input, node_attr, edge_attr, edge_src, edge_dst, edge_vec = self.preprocess(data)
+        del data
+
+        # Edge attributes
+        edge_sh = o3.spherical_harmonics(range(self.lmax + 1), edge_vec, True, normalization='component')
+        edge_attr = torch.cat([edge_attr, edge_sh], dim=1)
+
+        # Edge length embedding
+        edge_length = edge_vec.norm(dim=1)
+        edge_length_embedding = soft_one_hot_linspace(
+            edge_length,
+            0.0,
+            self.max_radius,
+            self.number_of_basis,
+            basis='smooth_finite',  # the smooth_finite basis with cutoff = True goes to zero at max_radius
+            cutoff=True,  # no need for an additional smooth cutoff
+        ).mul(self.number_of_basis**0.5)
+
+        node_outputs = self.mp(node_input, node_attr, edge_src, edge_dst, edge_attr, edge_length_embedding)
+
+        if self.pool_nodes:
+            return scatter(node_outputs, batch[...,None], dim=0).div(self.num_nodes**0.5)
+        else:
+            return node_outputs
+
--- train_e3nn.py
+++ train_e3nn.py
@@ -2,6 +2,7 @@ from typing import Dict
 import sys
 
 import torch
+from torch import optim
 from torch.utils.data import TensorDataset, DataLoader
 
 from sklearn.model_selection import train_test_split
@@ -11,10 +12,11 @@ from neural_slinky import e3nn_models
 
 device = "cpu"
 
-key = "cartesian_alpha_single"
+# key = "cartesian_alpha_single"
+key = "cartesian"
 batch_size = 1024
 num_epochs = 100
-lr = 1e-1
+lr = 1e-2
 weight_decay = 0
 
 # * Load Douglas data
@@ -24,7 +26,6 @@ douglas_dataset_dict: Dict[str, Dict[str, torch.Tensor]] = torch.load(
 
 input_tensor = (
     douglas_dataset_dict["coords"][key]
-    .flatten(start_dim=0, end_dim=1)
     .float()
     .clone()
     .detach()
@@ -32,7 +33,6 @@ input_tensor = (
 )
 output_tensor = (
     douglas_dataset_dict["force"][key]
-    .flatten(start_dim=0, end_dim=1)
     .float()
     .clone()
     .detach()
@@ -51,7 +51,7 @@ train_input, val_input, train_output, val_output = train_test_split(
 
 # TODO: do random rotation for augmentation
 
-train_dataset = TensorDataset(train_input[:batch_size], train_output[:batch_size])
+train_dataset = TensorDataset(train_input, train_output)
 val_dataset = TensorDataset(val_input, val_output)
 test_dataset = TensorDataset(test_input, test_output)
 
@@ -60,55 +60,80 @@ val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
 test_dataloader = DataLoader(test_dataset, batch_size=batch_size)
 
 
-model = e3nn_models.SlinkyForcePredictor(
-    irreps_node_input="0o",
-    irreps_node_attr="0o",
-    irreps_node_output="1x1o+1x0o",
+model = e3nn_models.SlinkyForcePredictorCartesian(
+    irreps_node_input="0e",
+    irreps_node_attr="2x0e",
+    irreps_edge_attr="2x0e",
+    irreps_node_output="1x1o",
     max_radius=0.06,
     num_neighbors=1,
     num_nodes=2,
     mul=50,
     layers=3,
     lmax=2,
+    pool_nodes=False
 )
+print(model)
 optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
 
 for epoch in range(num_epochs):
     print(f"epoch {epoch}")
     for (
-        cartesian_alpha,
+        cartesian,
         force,
     ) in train_dataloader:  # cartesian_alpha.shape == (batch_size, 6)
         # cartesian_alpha = cartesian_alpha[0]
         # force = force[0]
         # print(cartesian_alpha)
 
-        xy_1 = torch.cat(
-            [cartesian_alpha[..., 0:2], torch.zeros(cartesian_alpha.shape[:-1] + (1,))],
-            dim=-1,
-        )
-        alpha_1 = cartesian_alpha[..., 2:3]
-        xy_2 = torch.cat(
-            [cartesian_alpha[..., 3:5], torch.zeros(cartesian_alpha.shape[:-1] + (1,))],
-            dim=-1,
-        )
-        alpha_2 = cartesian_alpha[..., 5:6]
+        # xy_1 = torch.cat(
+        #     [cartesian_alpha[..., 0:2], torch.zeros(cartesian_alpha.shape[:-1] + (1,))],
+        #     dim=-1,
+        # )
+        # alpha_1 = cartesian_alpha[..., 2:3]
+        # xy_2 = torch.cat(
+        #     [cartesian_alpha[..., 3:5], torch.zeros(cartesian_alpha.shape[:-1] + (1,))],
+        #     dim=-1,
+        # )
+        # alpha_2 = cartesian_alpha[..., 5:6]
 
-        # node_input = torch.cat([xy_1, xy_2, alpha_1, alpha_2], dim=-1).to(device)
-        node_pos = torch.stack([xy_1, xy_2], dim=-1).to(device)
-        bar_alpha = torch.cat([alpha_1, alpha_2], dim=-1).to(device)
+        # # node_input = torch.cat([xy_1, xy_2, alpha_1, alpha_2], dim=-1).to(device)
+        # node_pos = torch.stack([xy_1, xy_2], dim=-1).to(device)
+        # bar_alpha = torch.cat([alpha_1, alpha_2], dim=-1).to(device)
 
+        num_triplets = cartesian.shape[0]
+        batch = torch.arange(num_triplets, device=device).view(-1, 1).repeat((1, 9)).flatten().long()
+        edge_index = torch.tensor(
+            [
+                [1,0], [1,2],
+                [4,3], [4,5],
+                [7,6], [7,8],
+                [1,4], [4,7]
+            ], device=device
+        ).repeat((num_triplets,1)) + torch.arange(num_triplets, device=device).mul_(9).repeat_interleave(8).view(-1,1)
+        edge_index = edge_index.long()
+
+        edge_attr = torch.nn.functional.one_hot(torch.tensor([1,1,1,1,1,1,0,0]).repeat(num_triplets), num_classes=2).float()
+
+        node_attr = torch.nn.functional.one_hot(torch.tensor([1,0,1]).repeat(3*num_triplets), num_classes=2).float()
+
+        cartesian = cartesian.reshape(-1, 2)
+        node_input = cartesian.new_ones(cartesian.shape[0], 1)
+        data = {
+            "pos": torch.cat([cartesian, cartesian.new_zeros(cartesian.shape[0], 1)], dim=-1),
+            "edge_src": edge_index[:,0],
+            "edge_dst": edge_index[:,1],
+            "node_input": node_input,
+            "batch": batch,
+            "node_attr": node_attr,
+            "edge_attr": edge_attr,
+        }
         with torch.enable_grad():
-            output = model(node_pos, bar_alpha)
-        output = output.reshape(-1, 2, output.shape[-1])
-        force_xy_1 = output[..., 0, 0:2]
-        force_xy_2 = output[..., 1, 0:2]
-        force_alpha_1 = output[..., 0, 3:4]
-        force_alpha_2 = output[..., 1, 3:4]
-        force_predicted = torch.cat(
-            [force_xy_1, force_alpha_1, force_xy_2, force_alpha_2], dim=-1
-        )
-        mse = (force - force_predicted).norm(dim=0)
+            output = model(data)
+        # print(output.shape)
+        # print(torch.autograd.grad(output[0,0], list(model.parameters()), retain_graph=True))
+        # exit()
+        mse = (force.view(-1,2) - output[:,0:2]).norm(dim=0)
         loss = mse.sum()
 
         sys.stdout.write(f"\rBatch loss: {loss.item()}")
@@ -116,9 +141,10 @@ for epoch in range(num_epochs):
         optimizer.zero_grad()
         loss.backward()
 
-        for n, p in model.named_parameters():
-            print(n)
-            # print(p)
-            print(p.grad)
+        # for n, p in model.named_parameters():
+        #     print(n)
+        #     # print(p)
+        #     print(p.grad)
 
         optimizer.step()
+    print()
