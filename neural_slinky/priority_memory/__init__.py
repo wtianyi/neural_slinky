d562456114ed5a5d5b99f942aebfe7e7dc56e336
Tianyi Wang <wtianyi1204@gmail.com>
2022-02-11 16:23:47 -0800

    Remove the slower version of prioritized replay

--- neural_slinky/priority_memory/__init__.py
+++ neural_slinky/priority_memory/__init__.py
@@ -1,4 +1,3 @@
-from .priority_memory import FastPriorReplayBuffer, SegmentTree
 from .prio import PrioritizedReplayBuffer, PrioritizedReplayBetaScheduler
 from .batch import Batch
 # name = 'priority_memory_buffer'
